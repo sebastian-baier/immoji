71922d1b3c6e1eaff3bf74c6adedcad1ba2d529d
applecakeman <sebastian@baier.codes>
2024-10-27 02:05:30 +0200

    delete not necessary code

--- src/app/property/create/page.tsx
+++ src/app/property/create/page.tsx
@@ -1,8 +1,11 @@
 'use server';
+
 import Stepper from '@/containers/property/create-property';
-import { CreateTest } from '@/containers/property/create-test';
 
 export default async function Create() {
-
-    return <div className='flex flex-col gap-6 items-start h-full'><Stepper></Stepper></div>;
+  return (
+    <div className="flex flex-col gap-6 items-start h-full">
+      <Stepper></Stepper>
+    </div>
+  );
 }
--- src/app/property/page.tsx
+++ src/app/property/page.tsx
@@ -1,26 +1,16 @@
 'use server';
 
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 import { Button } from '@/components/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/dialog';
 import { Icons } from '@/components/icons';
 
+import { getCurrentSession } from '@/lib/auth/session';
 
 import { getPropertiesOfOwner } from '@/actions/property/get-properties-of-owner';
-import CreatePropertyForm from '@/containers/property/create-property-form';
 import { PropertyCard } from '@/containers/property/property-card';
 import { PropertyWithDetails } from '@/types/property';
-import Link from 'next/link';
-import { getCurrentSession } from '@/lib/auth/session';
 
 export default async function Property() {
   const { user } = await getCurrentSession();
@@ -36,48 +26,29 @@ export default async function Property() {
       <h2 className="text-xl font-semibold">Immobilien</h2>
 
       {/* todo add filter */}
-      <div className='grid grid-cols-2 gap-4 items-center justify-center'>
+      <div className="grid grid-cols-2 gap-4 items-center justify-center">
         {properties.map((property) => (
           <PropertyCard
             key={property.id}
             property={{
               ...property,
               additionalCosts: +property.additionalCosts,
-              rentValue: +property.rentValue
+              rentValue: +property.rentValue,
             }}
           />
         ))}
-        <div className='flex justify-center items-center'>
-
+        <div className="flex justify-center items-center">
           <Button
             variant={'icon'}
-            size='icon'
-            className='rounded-full p-2'
-            aria-label='add-property'
+            size="icon"
+            className="rounded-full p-2"
+            aria-label="add-property"
             asChild
-
           >
             <Link href={'/property/create'}>
               <Icons.plus size={2.5} />
             </Link>
           </Button>
-
-          {/* <Dialog>
-						<DialogTrigger asChild>
-						</DialogTrigger>
-						<DialogContent className='xl:max-w-[512px]'>
-							<DialogHeader>
-								<DialogTitle>
-									Legen Sie Ihre Immobilien an
-								</DialogTitle>
-							</DialogHeader>
-							<DialogDescription>
-								Bitte füllen Sie das folgende Formular aus, um
-								eine neue Immobilie anzulegen.
-							</DialogDescription>
-							<CreatePropertyForm userId={user.id} />
-						</DialogContent>
-					</Dialog> */}
         </div>
       </div>
     </div>
