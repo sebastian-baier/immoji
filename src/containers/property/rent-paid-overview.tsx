c22dcf749a94d64a4c9a52f757e9b86d56d74193
applecakeman <sebastian@baier.codes>
2024-11-29 15:13:20 +0100

    responsive rent-paid-overview

--- src/containers/property/rent-paid-overview.tsx
+++ src/containers/property/rent-paid-overview.tsx
@@ -1,5 +1,4 @@
 import { CheckIcon, ChevronRightIcon } from 'lucide-react'
-import { Button } from '@/components/custom-ui/button'
 import { AnimatedSubscribeButton } from '@/components/ui/animated-subscribe-button'
 import {
 	Card,
@@ -27,34 +26,38 @@ export function RentPaidOverview() {
 	return (
 		<Card>
 			<CardHeader>
-				<CardTitle className='text-md font-semibold text-gray-600'>
+				<CardTitle className='text-base font-semibold text-gray-600 sm:text-lg md:text-xl'>
 					Miete 2024
 				</CardTitle>
 			</CardHeader>
-			<CardContent className='flex flex-row gap-6'>
-				{Months.map((month) => (
-					<div
-						className='flex min-w-28 flex-col items-center justify-center gap-3'
-						key={month}
-					>
-						<label htmlFor=''>{month}</label>
-						<AnimatedSubscribeButton
-							subscribeStatus={false}
-							initialText={
-								<span className='group inline-flex items-center'>
-									Erhalten{' '}
-									<ChevronRightIcon className='ml-1 size-4 transition-transform duration-300 group-hover:translate-x-1' />
-								</span>
-							}
-							changeText={
-								<span className='group inline-flex items-center'>
-									<CheckIcon className='size-4' />
-								</span>
-							}
-							changeButtonColor={'bg-green-600'}
-						/>
-					</div>
-				))}
+			<CardContent>
+				<div className='grid grid-cols-12 gap-1 sm:gap-2'>
+					{Months.map((month) => (
+						<div
+							className='flex flex-col items-center justify-center'
+							key={month}
+						>
+							<label className='mb-1 text-center font-medium sm:text-xs md:text-sm'>
+								{month}
+							</label>
+							<AnimatedSubscribeButton
+								subscribeStatus={false}
+								initialText={
+									<span className='group inline-flex items-center sm:text-xs md:text-sm'>
+										Erhalten{' '}
+										<ChevronRightIcon className='ml-0.5 h-3 w-3 transition-transform duration-300 group-hover:translate-x-0.5 sm:h-3.5 sm:w-3.5 md:h-4 md:w-4' />
+									</span>
+								}
+								changeText={
+									<span className='group inline-flex items-center'>
+										<CheckIcon className='h-3 w-3 sm:h-3.5 sm:w-3.5 md:h-4 md:w-4' />
+									</span>
+								}
+								changeButtonColor={'bg-green-600'}
+							/>
+						</div>
+					))}
+				</div>
 			</CardContent>
 		</Card>
 	)
